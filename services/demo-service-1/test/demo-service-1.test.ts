d78ebebd2228ad102ea0a9f17d9e59b7cab44091
Frank Liu <frankliu81@gmail.com>
2022-12-31 13:26:18 -0800

    demo-service-1/cdk init app --language typescript

---
+++ services/demo-service-1/test/demo-service-1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DemoService1 from '../lib/demo-service-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-service-1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DemoService1.DemoService1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
